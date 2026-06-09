9e8fdf9c5c16666f822491c4f7dae498f52e5f85
Gargi Negi <garginegi310@gmail.com>
2026-06-09 22:34:51 +0530

    Add files via upload

---
+++ day 1/Q4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n, count =0;
+    printf("enter the value of n");
+    scanf("%d",&n);
+    while(n>0)
+    {
+        n =n/10;
+        count++;
+    
+    }
+    printf("number of digits is %d",count);
+    return 0;
+}
