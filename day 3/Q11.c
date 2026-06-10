577a1250eeeff2483593aabe709b737f1a34ed0b
Gargi Negi <garginegi310@gmail.com>
2026-06-10 12:49:15 +0530

    Add files via upload

---
+++ day 3/Q11.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int num1, num2,gcd;
+    printf ("enter two numbers:");
+    scanf ("%d %d" , &num1, &num2);
+    for(int i=1; i<=num1 && i<=num2; i++)
+    {
+        if(num1%i==0 && num2%i==0)
+            gcd = i;
+    }
+    printf("GCD of %d and %d is %d", num1, num2, gcd);
+    return 0;
+}
