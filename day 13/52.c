148e344e537170493bbb3a77b41d834671ab6ffa
Gargi Negi <garginegi310@gmail.com>
2026-06-13 22:26:30 +0530

    Add files via upload

---
+++ day 13/52.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[10], n, i;
+    int even = 0, odd = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+
+        if(arr[i] % 2 == 0)
+        {
+            even++;
+        }
+        else
+        {
+            odd++;
+        }
+    }
+
+    printf("Number of even elements = %d\n", even);
+    printf("Number of odd elements = %d", odd);
+
+    return 0;
+}
