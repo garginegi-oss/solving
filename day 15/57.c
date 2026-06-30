b791be52f9a3bf2475d9c09c5eeec14801ed78bd
Gargi Negi <garginegi310@gmail.com>
2026-06-30 20:15:31 +0530

    Add files via upload

---
+++ day 15/57.c
@@ -0,0 +1,27 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], n;
+    int i;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Reversed array:\n");
+
+    for(i = n - 1; i >= 0; i--)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
---
+++ day 15/60.c
@@ -0,0 +1,38 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], n;
+    int i, j = 0, temp;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        if(arr[i] != 0)
+        {
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            j++;
+        }
+    }
+
+    printf("Array after moving zeroes:\n");
+
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
